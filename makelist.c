c0f1ad0d6707b9b8dd4e61c7f3b24175bcd624f5
Rajeev Atla <rajeev@rajeevatla.com>
2024-04-05 01:02:43 +0000

    fix validation for arguments

--- makelist.c
+++ makelist.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]){
     if(argc != 3){
-        printf("Usage: %s <low> <high> <output file>\n", argv[0]);
+        printf("Usage: %s <low> <high> \n", argv[0]);
         return 1;
     }
 
